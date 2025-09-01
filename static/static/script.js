908732ccfeb3e5433ecd0d8b754603865dffeb89
Harsh-Kaithwas <kaithwasji.harsh@gmail.com>
2025-09-01 19:09:17 +0530

    Create script.js

---
+++ static/static/script.js
@@ -0,0 +1,12 @@
+function openDrawer(id) {
+    document.getElementById('drawer-' + id).classList.add('open');
+}
+function closeDrawer(id) {
+    document.getElementById('drawer-' + id).classList.remove('open');
+}
+function openAnalyzer(id) {
+    document.getElementById('popup-' + id).classList.add('open');
+}
+function closeAnalyzer(id) {
+    document.getElementById('popup-' + id).classList.remove('open');
+}
